1fd90aa0e50410526c82bb5b5b85369412a6be07
armcortex <armcortexfpga@gmail.com>
2023-08-23 15:39:50 +0800

    with revised prompt, output is more stable in format, but translation need to be improved

--- main.py
+++ main.py
@@ -14,10 +14,21 @@ from langchain.prompts.chat import (
 from prompt import system_prompt
 
 
-MODEL_BASE_PATH = os.environ.get('LLAMA_CPP_MODEL_PATH')
-# MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q4_K_S.bin'
+# TW Model
+MODEL_BASE_PATH = os.environ.get('LLAMA_CPP_MODEL_TW_PATH')
+MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q4_K_M.bin'
 # MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q4_K_M.bin'
-MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q4_0.bin'
+# MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q4_0.bin'
+# MODEL_NAME = '/Taiwan-LLaMa-13b-1.0.ggmlv3.q5_0.bin'
+
+# wizard-vicuna-13B
+# MODEL_BASE_PATH = os.environ.get('LLAMA_CPP_MODEL_BASE_PATH')
+# MODEL_NAME = '/wizard-vicuna/13B/Wizard-Vicuna-13B-Uncensored.ggmlv3.q4_K_M.bin'
+# MODEL_NAME = '/wizard-vicuna/13B/wizard-vicuna-13B.ggmlv3.q4_K_M.bin'
+
+# wizardlm-1.0
+# MODEL_NAME = '/wizardlm/13B/wizardlm-1.0-uncensored-llama2-13b.ggmlv3.q4_K_M.bin'
+
 MODEL_PATH = MODEL_BASE_PATH + MODEL_NAME
 
 PROMPT = './prompt/translate-zh-tw-prompt.txt'
@@ -35,8 +46,10 @@ def main():
         n_gpu_layers=1,
         n_threads=8,
         n_ctx=2048,
-        n_batch=512,
+        n_batch=32,
+        temperature=0,
         use_mlock=True,
+        stop=['</end>'],
         callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
         verbose=True,
     )
@@ -50,9 +63,9 @@ def main():
     chain = LLMChain(prompt=chat_prompt, llm=llm)
     
     texts = [
-        "\n24\n00:01:21,720 --> 00:01:23,400\n Just the question of, do you think it's for you?\nUser:",
-        "\n25\n00:01:23,400 --> 00:01:25,440\n And if you think it's for you, buy it.\nUser:",
-        "\n26\n00:01:25,440 --> 00:01:26,480\n It's a consumer product.\n User"]
+        "<Subtitle>:\n24\n00:01:21,720 --> 00:01:23,400\n Just the question of, do you think it's for you?",
+        "<Subtitle>:\n25\n00:01:23,400 --> 00:01:25,440\n And if you think it's for you, buy it.",
+        "<Subtitle>:\n26\n00:01:25,440 --> 00:01:26,480\n It's a consumer product."]
 
 
     t0 = time.perf_counter()
@@ -60,7 +73,7 @@ def main():
         chain.run(text=t)
 
     t1 = time.perf_counter()
-    print(f'Execute Time: {t1-t0:.2f} sec')
+    print(f'Execute Time: {t1-t0:.2f} sec, {(t1-t0)/60:.2f} min')
 
 
 if __name__ == '__main__':
